cd10d167621c6584110d18e9b81b2d7b84e63aef
Sathyanarayanan <110828645+Sathyanarayanan-R@users.noreply.github.com>
2023-07-28 21:53:30 +0530

    updated

--- src/screens/OrderScreen.js
+++ src/screens/OrderScreen.js
@@ -33,7 +33,7 @@ const OrderScreen = ({ match, history }) => {
         const {data: clientId} = await axios.get('/api/config/paypal');
         const script = document.createElement('script');
         script.type = 'text/javascript'
-        script.src = `https://www.paypal.com/sdk/js?client-id=AU6Hdw7POgGHYRkTHDmQfgrwYjCZXgfGvV3Qo-0Hp5swDaNKRfnvdUCulOs0qIsDtaKwZFuSnntXtssH`
+        script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`
         script.async = true
         script.onload = () => {
           setSdkReady(true)
